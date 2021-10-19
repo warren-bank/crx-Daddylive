af772bec8101521e3d44e06d8a17997c70ab2786
Warren R Bank <warren.r.bank@gmail.com>
2021-10-18 21:36:48 -0700

    refactor options, add option to redirect WebMonkey to url after Intent

--- webmonkey-userscript/Daddylive.user.js
+++ webmonkey-userscript/Daddylive.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Daddylive
 // @description  Improve site usability. Watch videos in external player.
-// @version      1.0.1
+// @version      1.0.2
 // @match        *://daddylive.me/*
 // @match        *://*.daddylive.me/*
 // @match        *://eplayer.to/daddylive.php*
@@ -23,20 +23,26 @@
 // ----------------------------------------------------------------------------- constants
 
 var user_options = {
-  "redirect_to_webcast_reloaded": true,
-  "force_http":                   true,
-  "force_https":                  false,
-
-  "timeout_ms": {
-    "live_videostream":           0
+  "common": {
+    "timeout_ms": {
+      "live_videostream":           0
+    }
+  },
+  "webmonkey": {
+    "post_intent_redirect_to_url":  "about:blank"
+  },
+  "greasemonkey": {
+    "redirect_to_webcast_reloaded": true,
+    "force_http":                   true,
+    "force_https":                  false
   }
 }
 
 // ----------------------------------------------------------------------------- URL links to tools on Webcast Reloaded website
 
 var get_webcast_reloaded_url = function(video_url, vtt_url, referer_url, force_http, force_https) {
-  force_http  = (typeof force_http  === 'boolean') ? force_http  : user_options.force_http
-  force_https = (typeof force_https === 'boolean') ? force_https : user_options.force_https
+  force_http  = (typeof force_http  === 'boolean') ? force_http  : user_options.greasemonkey.force_http
+  force_https = (typeof force_https === 'boolean') ? force_https : user_options.greasemonkey.force_https
 
   var encoded_video_url, encoded_vtt_url, encoded_referer_url, webcast_reloaded_base, webcast_reloaded_url
 
@@ -69,7 +75,7 @@ var redirect_to_url = function(url) {
 
   if (typeof GM_loadUrl === 'function') {
     if (typeof GM_resolveUrl === 'function')
-      url = GM_resolveUrl(url, unsafeWindow.location.href)
+      url = GM_resolveUrl(url, unsafeWindow.location.href) || url
 
     GM_loadUrl(url, 'Referer', unsafeWindow.location.href)
   }
@@ -81,6 +87,19 @@ var redirect_to_url = function(url) {
       unsafeWindow.window.location = url
     }
   }
+}
+
+var process_webmonkey_post_intent_redirect_to_url = function() {
+  var url = null
+
+  if (typeof user_options.webmonkey.post_intent_redirect_to_url === 'string')
+    url = user_options.webmonkey.post_intent_redirect_to_url
+
+  if (typeof user_options.webmonkey.post_intent_redirect_to_url === 'function')
+    url = user_options.webmonkey.post_intent_redirect_to_url()
+
+  if (typeof url === 'string')
+    redirect_to_url(url)
 }
 
 var process_video_url = function(video_url, video_type, vtt_url, referer_url) {
@@ -107,9 +126,10 @@ var process_video_url = function(video_url, video_type, vtt_url, referer_url) {
     }
 
     GM_startIntent.apply(this, args)
+    process_webmonkey_post_intent_redirect_to_url()
     return true
   }
-  else if (user_options.redirect_to_webcast_reloaded) {
+  else if (user_options.greasemonkey.redirect_to_webcast_reloaded) {
     // running in standard web browser: redirect URL to top-level tool on Webcast Reloaded website
 
     redirect_to_url(get_webcast_reloaded_url(video_url, vtt_url, referer_url))
@@ -190,8 +210,8 @@ var init = function() {
   }
 
   if (is_inner_iframe) {
-    if (user_options.timeout_ms.live_videostream > 0)
-      setTimeout(process_live_videostream, user_options.timeout_ms.live_videostream)
+    if (user_options.common.timeout_ms.live_videostream > 0)
+      setTimeout(process_live_videostream, user_options.common.timeout_ms.live_videostream)
     else
       process_live_videostream()
     return
