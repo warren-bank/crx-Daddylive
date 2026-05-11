a1af70dae265a1dd9aa012e2bd8d39939ae2fe3a
Warren R. Bank <warren.r.bank@gmail.com>
2026-05-10 19:19:50 -0700

    v2.3.7: accomodate for changes to website

--- webmonkey-userscript/Daddylive.user.js
+++ webmonkey-userscript/Daddylive.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Daddylive
 // @description  Improve site usability. Watch videos in external player.
-// @version      2.3.6
+// @version      2.3.7
 // @include      /^https?:\/\/(?:[^\.\/]*\.)*(?:1ststream\.shop|247ovo\.lol|a1sports\.shop|apkship\.shop|arlive\.shop|beststreams\.shop|bfstv\.shop|bigsportz\.shop|bingsport\.shop|bizz-streams2u\.shop|bizzstream\.shop|buddycenter\.shop|buddycenters\.shop|buzzstream\.shop|crackstreamshd\.shop|cwcstreams\.com|cyclinsport\.shop|daddy-stream\.xyz|daddyhd\.shop|daddyislive\.online|daddylive\.mp|daddylive1\.ru|daddylive1\.shop|dailytechs\.shop|dlhd\.so|dlhd\.sx|doralive\.live|duplex-full\.shop|engstreams\.shop|f1streams\.lol|firestream4u\.shop|footballstreams\.lol|footyhunterhd\.shop|foxstream4u\.shop|freelivetvone\.xyz|freetvspor\.lol|freetvspor\.shop|fsportshd\.shop|gomstream\.info|hitsports\.shop|homosports\.shop|kingstreams\.shop|kingstreamss\.shop|klubsports\.buzz|klubsports\.fun|klubsports\.site|kofitv\.live|linesportz\.lol|liveplays\.shop|livesports2u\.shop|liveworld\.shop|miztv\.live|miztv\.shop|mudasir3u\.shop|nowagoal\.lol|one-stream\.shop|pandastreams\.shop|pandastreamz\.shop|poscitechs\.lol|poscitechs\.shop|poscitechs\.xyz|rainostream4u\.shop|rainostreams\.lol|rippleamu4\.shop|rippleamu4s\.shop|ripplestream2u\.shop|ripplestream4u\.shop|ripplestreams\.shop|ripplestreams2u\.shop|rockhd\.lol|soccer100\.shop|soccerhub\.lol|soccerstreams2\.click|socceryouknow\.shop|sooperstream4u\.shop|sports2watch\.shop|sportss\.shop|sportsslive\.shop|sportstreamslife\.shop|sportzlive\.shop|streamer4u\.shop|streamlight\.lol|stronstream\.shop|techtop3u\.shop|techttop\.shop|thebaldstreamer\.lol|thedaddy\.click|thedaddy\.to|thesport\.lol|tonnestreams\.shop|topstreamz\.shop|tripplestream\.com|tvtoss\.lol|unitedbacke\.shop|venushd\.click|viprow1\.shop|vipstreamer\.shop|vipstreamers\.shop|watchhdtv\.shop|worldsports4u\.shop|worldsportz4u\.shop|worldsstream\.shop|worldstreams\.lol|worldstreamz\.shop|www\.worldstreamz.shop|zenlic\.shop)\/.*$/
-// @include      /^https?:\/\/(?:[^\.\/]*\.)*(?:cookiewebplay|daddylive|daddylivehd|dlhd|dokoplay|gocast|jewelavid|maxsport|quest4play|radamel|sportkart|streamservicehd|thedaddy|weblivehdplay|zvision)\d*\.(?:buzz|click|com|fun|icu|info|link|live|lol|me|mp|one|online|ru|shop|site|so|sx|to|watch|xyz)\/.*$/
+// @include      /^https?:\/\/(?:[^\.\/]*\.)*(?:cookiewebplay|daddylive|daddylivehd|dlhd|dokoplay|gocast|jewelavid|jimpenopisonline|maxsport|quest4play|radamel|sportkart|streamservicehd|thedaddy|weblivehdplay|zvision)\d*\.(?:buzz|click|com|eu|fun|icu|info|link|live|lol|me|mp|one|online|pk|ru|shop|site|so|sx|to|watch|xyz)\/.*$/
 // @icon         https://i.imgur.com/8EL6mr3.png
 // @run-at       document-end
 // @grant        unsafeWindow
@@ -23,7 +23,7 @@ var user_options = {
     "enable_debug_alerts":          false,
     "emulate_webmonkey":            false,
     "hide_iframe":                  true,
-    "init_delay_ms":                0
+    "init_delay_ms":                1000
   },
   "webmonkey": {
     "post_intent_redirect_to_url":  "about:blank"
@@ -123,7 +123,7 @@ var get_webcast_reloaded_url = function(video_url, vtt_url, referer_url, force_h
 
   webcast_reloaded_base = {
     "https": "https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html",
-    "http":  "http://webcast-reloaded.surge.sh/index.html"
+    "http":  "http://webcast-reloaded.frii.site/index.html"
   }
 
   webcast_reloaded_base = (force_http)
@@ -345,13 +345,16 @@ var extract_dom_video_url_regexs = {
     video_url: /\s+source:\s*['"]([^'"]+\.m3u8[^'"]*)['"]/
   },
   v02: {
+    video_url_base64: /\s+source:\s*window\.atob\(\s*['"]([^'"]+)['"]\s*\)/
+  },
+  v03: {
     channel_id: /\s+channelKey\s*=\s*['"]([^'"]+)['"]/,
     video_url: {
       default_server_key: /['"](http[^'"]+)['"]\s*\+\s*channelKey\s*\+\s*['"]([^'"]+\.m3u8[^'"]*)['"]/,
       dynamic_server_key: /['"](http[^'"]+)['"]\s*\+\s*serverKey\s*\+\s*['"]([^'"]+)['"]\s*\+\s*serverKey\s*\+\s*['"]([^'"]+)['"]\s*\+\s*channelKey\s*\+\s*['"]([^'"]+\.m3u8[^'"]*)['"]/
     }
   },
-  v03: {
+  v04: {
     video_method: /[;\s]player\.load\s*\(\s*\{\s*source\s*:\s*(.+?)\s*\(\s*\)\s*,\s*mimeType\s*:\s*['"]([^'"]+)['"]\s*\}\s*\)/
   }
 }
@@ -383,6 +386,7 @@ var extract_dom_video_url = function(callback) {
     extract_dom_video_url_01(script, video_extractor_callback)
     extract_dom_video_url_02(script, video_extractor_callback)
     extract_dom_video_url_03(script, video_extractor_callback)
+    extract_dom_video_url_04(script, video_extractor_callback)
   }
 }
 
@@ -398,9 +402,20 @@ var extract_dom_video_url_01 = function(script, callback) {
 }
 
 var extract_dom_video_url_02 = function(script, callback) {
+  var match, video_url
+
+  match = extract_dom_video_url_regexs.v02.video_url_base64.exec(script)
+  if (match) {
+    video_url = unsafeWindow.atob(match[1]) + '#video.m3u8'
+  }
+
+  callback(video_url)
+}
+
+var extract_dom_video_url_03 = function(script, callback) {
   var match, channel_id
 
-  match = extract_dom_video_url_regexs.v02.channel_id.exec(script)
+  match = extract_dom_video_url_regexs.v03.channel_id.exec(script)
   if (match) {
     channel_id = match[1]
 
@@ -408,13 +423,13 @@ var extract_dom_video_url_02 = function(script, callback) {
       var video_url
 
       if (error || !data || (typeof data !== 'object') || !data.server_key || (data.server_key === 'top1/cdn')) {
-        match = extract_dom_video_url_regexs.v02.video_url.default_server_key.exec(script)
+        match = extract_dom_video_url_regexs.v03.video_url.default_server_key.exec(script)
         if (match) {
           video_url = match[1] + channel_id + match[2]
         }
       }
       else {
-        match = extract_dom_video_url_regexs.v02.video_url.dynamic_server_key.exec(script)
+        match = extract_dom_video_url_regexs.v03.video_url.dynamic_server_key.exec(script)
         if (match) {
           video_url = match[1] + data.server_key + match[2] + data.server_key + match[3] + channel_id + match[4]
         }
@@ -424,10 +439,10 @@ var extract_dom_video_url_02 = function(script, callback) {
   }
 }
 
-var extract_dom_video_url_03 = function(script, callback) {
+var extract_dom_video_url_04 = function(script, callback) {
   var match, video_method, video_mimetype, video_url
 
-  match = extract_dom_video_url_regexs.v03.video_method.exec(script)
+  match = extract_dom_video_url_regexs.v04.video_method.exec(script)
   if (match) {
     video_method   = match[1]
     video_mimetype = match[2]
